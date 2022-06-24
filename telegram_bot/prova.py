7efce9ad201bce04f1bb9669ecdb05f018d625dd
NicolisAlessandro <19131@studenti.marconiverona.edu.it>
2022-06-24 14:01:41 +0200

    bot

--- telegram_bot/prova.py
+++ telegram_bot/prova.py
@@ -1,10 +1,11 @@
 import sqlite3
+import math
 
 def descrizioneBot(parametro):
     conn = sqlite3.connect('veronacard.db')
     c = conn.cursor()   
     
-    query_par = f'SELECT name_id FROM sites_info WHERE category_it == "Chiese"'    
+    query_par = f'SELECT descr_it FROM sites_info WHERE category_it == "Chiese"'    
     
     c.execute(query_par)
     data = c.fetchall()
@@ -26,7 +27,7 @@ def AffluenzaBot():
 
 #AffluenzaBot("Chiese")
 
-def posizioneBot(parametro):
+#def posizioneBot(parametro):
     conn = sqlite3.connect('veronacard.db')
     c = conn.cursor()   
     
@@ -47,9 +48,13 @@ def descrizioneBot(parametro):
     
     c.execute(query_par)
     data = c.fetchall()
-    #for row in data:
-     #   print(row)
+
+    for row in data:
+        print(row) 
+    
     return data
+
+descrizioneBot("Chiese")
 
 def AffluenzaBot():
     conn = sqlite3.connect('bruh.db')
@@ -65,17 +70,61 @@ def AffluenzaBot():
 
 #AffluenzaBot("Chiese")
 
-def posizioneBot(parametro):
+def posizioneBoty(parametro):
     conn = sqlite3.connect('veronacard.db')
     c = conn.cursor()   
     
-    query_par = f'SELECT longitude, latitude FROM sites_info WHERE category_it == "Monumenti"'    
+    query_par = f'SELECT longitude FROM sites_info WHERE category_it == "Monumenti"'    
     
     c.execute(query_par)
     data = c.fetchall()
-
-    for row in data:
-        print(row)  
+    print (data[2])
+    #for row in data:
+    #    print(row)  
     return data
 
-#posizioneBot("Monumenti")
+posizioneBoty("Monumenti")
+
+def posizioneBotx(parametro):
+    conn = sqlite3.connect('veronacard.db')
+    c = conn.cursor()   
+    
+    query_par = f'SELECT latitude FROM sites_info WHERE category_it == "Monumenti"'    
+    
+    c.execute(query_par)
+    data = c.fetchall()
+    
+    print (data[2])
+    #for row in data:
+    #    print(row)  
+    return data
+
+posizioneBotx("Monumenti")
+
+#def reply_location(paramtero):
+    #conn = sqlite3.connect('veronacard.db')
+    #c = conn.cursor()   
+    
+    #query_par = f'SELECT longitude, latitude FROM sites_info WHERE category_it == "Monumenti"'
+    
+    #c.execute(query_par)
+    #data = c.fetchall()
+    
+    #Ox=0.0; Oy=0.0
+    #Ax=3.0; Ay=4.0
+    #Bx=4.0; By=3.0
+ 
+    #dOA=math.sqrt((Ox-Ax)**2 + (Oy-Ay)**2)
+    #dOB=math.sqrt((Ox-Bx)**2 + (Oy-By)**2)
+    #dAB=math.sqrt((Ax-Bx)**2 + (Ay-By)**2)
+ 
+    #print("OA =",dOA)
+    #print("OB =",dOB)
+    #print("AB =",dAB)
+    
+    #for row in data:
+        #print(row)  
+    #return data
+
+#reply_location("paramtero")
+
